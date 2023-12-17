82768925115bc05541c55c5b17df13cc09413cee
manasjhamj <jhamanas939@gmail.com>
2023-12-17 16:52:43 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+VanillaTilt.init(document.querySelectorAll(".glassmorphic-card"), {
+    max: 15,
+    speed: 200,
+    glare: true,
+    "max-glare": 1,
+});
+
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.heading', { delay: 1300 });
+ScrollReveal().reveal('.heading2', { delay: 2000 });
+ScrollReveal().reveal('.card', { delay: 700 });
